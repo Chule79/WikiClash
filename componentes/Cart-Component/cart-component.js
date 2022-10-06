fb472183bb85641b5ccaef74a163cb8bcc3ca260
Chule79 <jsantoslopez99@gmail.com>
2022-10-06 15:51:34 +0200

    conexion terminada

---
+++ componentes/Cart-Component/cart-component.js
@@ -0,0 +1,15 @@
+
+
+
+export const cartaConstr = (imagen, name, levelMax) => {
+    return `
+        <div>
+            <div>
+                <img src="${card.imagen}"></img>
+            </div>
+            <h2>${card.name}</h2>
+            <p>Nivael maximo alcanzable ${card.levelMax}</p>
+        </div>
+    `
+
+}
--- main.js
+++ main.js
@@ -1,23 +1,48 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import {cartaConstr} from "./componentes/Cart-Component/cart-component.js"
+const wall = document.querySelector("#wall")
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+
+const getData = async () => {
+  try {
+    const response = await fetch ("http://localhost:3000/items");
+    const transformData = await response.json();
+    mapData(transformData); 
+  } catch (error) {
+    console.log(error);
+  }
+  
+} 
+
+
+
+const mapData = (cards) => {
+  let elementsCard = cards.map((card) => {
+    return {
+      name: card.name,
+      levelMax: card.maxLevel,
+      imagen: card.iconUrls.medium
+    }
+  })
+  
+  drawCard(elementsCard)
+}
+
+const drawCard = (cards) => {
+  cards.forEach(card => {
+    const cartaCreate = `
+    <div>
+        <div>
+            <img src="${card.imagen}"></img>
+        </div>
+        <h2>${card.name}</h2>
+        <p>Nivael maximo alcanzable ${card.levelMax}</p>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+`;
+    wall.innerHTML += cartaCreate;
+  });
+}
 
-setupCounter(document.querySelector('#counter'))
+
+
+getData()
